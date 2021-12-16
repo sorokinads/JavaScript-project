2de961534be675ccfc20b5d9de24de0dd23265a2
Daria <msdashaback@gmail.com>
2021-12-16 13:50:09 +0300

    Практика, ч.1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 "use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres:[],
+    privat: false
+};
+
+const film = prompt('Один из последних просмотренных фильмов?', ''),
+      rate = prompt('На сколько оцените его?', ''),
+      film2 = prompt('Один из последних просмотренных фильмов?', ''),
+      rate2 = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[film] = rate;
+personalMovieDB.movies[film2] = rate2;
+
+console.log(personalMovieDB);
